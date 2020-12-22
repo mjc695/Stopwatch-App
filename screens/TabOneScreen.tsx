b61acae81d3e03d73ee7f6299cd49fb4627846df
Monferd <monferd.j.colin@gmail.com>
2020-12-22 14:12:31 -0500

    start/stop feature added

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,15 +1,43 @@
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import {useState, useEffect} from 'react'
+import { StyleSheet, Button } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 
 export default function TabOneScreen() {
+  let [stopWatch, setStopWatch] = useState(0)
+  const [toggle, setToggle] = useState(false)
+
+
+  useEffect(()=>{
+    let intervalFunc
+    if (toggle) {
+      intervalFunc = setInterval(()=>{
+        setStopWatch(stopWatch=> stopWatch += .1)   
+      }, 100)
+    }
+    return ()=> clearInterval(intervalFunc)
+  },
+    [toggle]
+  )
+
+  const handleChange = () =>{
+    setToggle(!toggle)
+  }
+
+
+  
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.js" />
+      {/* <Text style={styles.title}>Tab One</Text> */}
+      {/* <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <EditScreenInfo path="/screens/TabOneScreen.js" /> */}
+      <Text style={styles.timer}>
+        stop watch: {stopWatch.toFixed(1)}s
+      </Text>
+      <Button title={`${toggle? 'Stop' : 'Start'}`} onPress={()=> setToggle(!toggle)} />
     </View>
   );
 }
@@ -29,4 +57,7 @@ const styles = StyleSheet.create({
     height: 1,
     width: '80%',
   },
+  timer: {
+    fontSize: 30
+  }
 });
